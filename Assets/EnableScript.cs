f1b08b96af1a451669f27c01b254d72e98826da0
MatheusAThomaz <matheus.thomaz@dcomp.sor.ufscar.br>
2018-08-16 01:47:06 -0300

    Moving barrel

---
+++ Assets/EnableScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableScript : MonoBehaviour {
+
+	public GameObject object_game;
+
+	// Use this for initialization
+	void Start () {
+			MoveToTarget script;
+      
+    		script = object_game.GetComponent<MoveToTarget>();
+     		script.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey("space")) {
+			MoveToTarget script;
+    		script = object_game.GetComponent<MoveToTarget>();
+     		script.enabled = true;
+		}
+	}
+}
---
+++ Assets/MoveBarrel.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBarrel : MonoBehaviour {
+	
+	int speed = 10;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		 if (Input.GetButton("Vertical")) {
+			 if(Input.GetAxis("Vertical") > 0) {
+				transform.Translate(new Vector3(0, speed, 0) * Time.deltaTime);
+			 }
+			 else {
+				transform.Translate(new Vector3(0, -speed, 0) * Time.deltaTime);
+			 }
+		 }
+
+		 if (Input.GetButton("Horizontal")) {
+			 if(Input.GetAxis("Horizontal") > 0) {
+				transform.Translate(new Vector3(speed, 0 ,0) * Time.deltaTime);
+			 }
+			 else {
+				transform.Translate(new Vector3(-speed, 0 ,0) * Time.deltaTime);
+			 }
+		 }
+
+		if (Input.GetButton("Deep")) {
+			 if(Input.GetAxis("Deep") > 0) {
+				 Debug.Log("pos");
+				transform.Translate(new Vector3(0, 0 ,speed) * Time.deltaTime);
+			 }
+			 else {
+				 				 Debug.Log("neg");
+
+				transform.Translate(new Vector3(0, 0 ,-speed) * Time.deltaTime);
+			 }
+		 }
+ 
+	}
+}
